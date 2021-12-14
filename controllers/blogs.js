3b7f9b80e04de372c88358d0138056abe14b6b3a
Your Name <you@example.com>
2021-12-14 09:27:34 -0500

    submission for 4.22

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,18 +5,19 @@ const {info,errorInfo} = require('../utils/logger')
 const blogsRouter = express.Router()
 const _ = require('lodash')
 const jwt = require('jsonwebtoken')
+const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/',async (req,res)=>{
   const blogs = await Blog.find({}).populate('user',{name:1,username:1,id:1})
   res.status(200).json(blogs)
 })
 
-blogsRouter.post('/',async (req,res)=>{
-  const tokenInfo = res.locals.tokenInfo 
-  if(!tokenInfo || !tokenInfo.id) {
-    return res.status(401).json({error: 'token missing or invalid'})
+blogsRouter.post('/',userExtractor,async (req,res)=>{
+  if(!req.user) {
+    return res.status(401).json({error:'not a valid user to create blog'})
   }
-  let blogData = {...req.body,user:tokenInfo.id}
+  const user = req.user
+  let blogData = {...req.body,user:user._id.toString()}
 
   const requiredProps = ['title','author','url','user']
   const inter = _.intersectionWith(requiredProps,Object.keys(blogData),_.isEqual)
@@ -34,9 +35,8 @@ blogsRouter.post('/',async (req,res)=>{
     res.status(400).end()
     return
   }
-  const user = await User.findById(blogData.user)
-  const {userId,...blogData1} = {...blogData,user: user.id}
-  const blog = new Blog(blogData1)
+
+  const blog = new Blog(blogData)
 
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog.id)
@@ -44,14 +44,10 @@ blogsRouter.post('/',async (req,res)=>{
   res.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id',async (req,res)=>{
-  const tokenInfo = res.locals.tokenInfo 
-  if(!tokenInfo || !tokenInfo.id) {
-    return res.status(401).json({error: 'token missing or invalid'})
-  }
+blogsRouter.delete('/:id',userExtractor,async (req,res)=>{
   const blogId = req.params.id  
   const blog = await Blog.findById(blogId)
-  if(blog.user.toString()!==tokenInfo.id) {
+  if(!req.user || (blog.user.toString()!==req.user._id.toString())) {
     return res.status(400).json({error:'blog was created by a different user'})
   }
   const result = await Blog.findByIdAndDelete(blogId)
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,6 @@
 const logger = require('./logger')
 const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const errorHandler = (error,req,res,next)=>{
   const errdata = Object.entries(error)
@@ -22,7 +23,26 @@ const tokenInfoExtractor = (req,res,next)=>{
   next()
 }
 
+const userExtractor = async (req,res,next)=>{
+  let user = null
+  try {
+    const authorization = req.get('authorization')   
+    if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      const token =  authorization.substring(7)
+      const tokenInfo = jwt.verify(token,process.env.SECRET)
+      if(tokenInfo.id) {
+        user = await User.findById(tokenInfo.id)
+      }
+    }
+  } catch(error) {
+    logger.errorInfo('user extractor error',error)
+  }
+  req.user = user
+  next()
+}
+
 module.exports = {
   errorHandler,
-  tokenInfoExtractor
+  tokenInfoExtractor,
+  userExtractor
 }
